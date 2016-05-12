8a373b394f057601a476915bb8a5d58d357da2d3
ravenesque1 <ravenesque2@gmail.com>
2016-05-12 09:21:55 -0700

    Initial Commit

---
+++ Couchy/Couchy.h
@@ -0,0 +1,19 @@
+//
+//  Couchy.h
+//  Couchy
+//
+//  Created by Raven Smith on 5/12/16.
+//  Copyright © 2016 Raven Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Couchy.
+FOUNDATION_EXPORT double CouchyVersionNumber;
+
+//! Project version string for Couchy.
+FOUNDATION_EXPORT const unsigned char CouchyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Couchy/PublicHeader.h>
+
+
