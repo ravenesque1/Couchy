de5e123f1ab17b225847b4e0b741b72b2527e981
ravenesque1 <ravenesque2@gmail.com>
2016-05-16 09:38:00 -0700

    Add CouchyController

---
+++ Couchy/CouchyController.h
@@ -0,0 +1,15 @@
+//
+//  CouchyController.h
+//  Couchy
+//
+//  Created by Raven Smith on 5/16/16.
+//  Copyright © 2016 Raven Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CouchyController : UIViewController
+
+@property (strong, nonatomic) UILabel* name;
+
+@end
---
+++ Couchy/CouchyController.m
@@ -0,0 +1,44 @@
+//
+//  CouchyController.m
+//  Couchy
+//
+//  Created by Raven Smith on 5/16/16.
+//  Copyright © 2016 Raven Smith. All rights reserved.
+//
+
+#import "CouchyController.h"
+
+@implementation CouchyController
+
+-(void)viewDidLoad {
+    self.view.backgroundColor = [UIColor orangeColor];
+    [self setup];
+}
+
+
+-(void)setup {
+    self.name = [UILabel new];
+    self.name.text = @"Couchy v1.1";
+    [self.name sizeToFit];
+    [self.view addSubview:self.name];
+    
+    NSLayoutConstraint *hConstraint = [NSLayoutConstraint constraintWithItem:self.name
+                                                                   attribute:NSLayoutAttributeCenterX
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:self.view
+                                                                   attribute:NSLayoutAttributeCenterX
+                                                                  multiplier:1.0
+                                                                    constant:0];
+    
+    NSLayoutConstraint *vConstraint = [NSLayoutConstraint constraintWithItem:self.name
+                                                                   attribute:NSLayoutAttributeCenterY
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:self.view
+                                                                   attribute:NSLayoutAttributeCenterY
+                                                                  multiplier:1.0
+                                                                    constant:0];
+    
+    [self.view addConstraints:@[hConstraint, vConstraint]];
+}
+
+@end
